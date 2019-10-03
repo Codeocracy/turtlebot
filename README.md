79e1d48bc6c27b768a80bd87c2cc6c3cdc33e3d5
Lars Borchert <40208002+Lars314@users.noreply.github.com>
2019-10-03 12:58:46 -0700

    Basic feature update 1
    Added ability to ask for a turtle picture

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # turtlebot
 A glorious turtle bot to bless your servers with
+
+Key Functions:
+
+- Send pictures of turtles with the /turtle command
+- React using the turtle emoji to random stuff
+- Be overall awesome
---
+++ turtleBot.js
@@ -0,0 +1,24 @@
+var Discord = require("discord.io");
+var config = require(".config.json");
+var fs = require('fs');
+var dire = fs.readdirSync('./TurtlePics');
+
+var bot = new Discord.Client({
+    token: config.token,
+    autorun: true
+})
+
+function newTurtlePic() {
+    var index = Math.floor(Math.random()*dire.length);
+    return dire[index];
+}
+
+bot.on('message', function (user, userID, channelID, message, evt) {
+	if (userID == bot.id) {
+		return;
+    }
+    if (message.startsWith("/turtle")) {
+        message.channel.send({ files: ['./TurtlePics/' + newTurtlePic()]});
+        return;
+    }  
+});
